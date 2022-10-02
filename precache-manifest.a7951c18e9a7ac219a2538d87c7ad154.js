95c18c84a9c8781f2f8b8d02d44aed4374aca03a
preet <preetpatelofficial004@gmail.com>
2022-10-02 17:18:29 +0530

    Updates

---
+++ precache-manifest.a7951c18e9a7ac219a2538d87c7ad154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52fdf3659b3d8e5c4474",
+    "url": "/Texters/static/css/main.ce0cd084.chunk.css"
+  },
+  {
+    "revision": "52fdf3659b3d8e5c4474",
+    "url": "/Texters/static/js/main.021fbcb5.chunk.js"
+  },
+  {
+    "revision": "f19f4e94bfb2efef9b68",
+    "url": "/Texters/static/js/runtime~main.bb0c0ab1.js"
+  },
+  {
+    "revision": "58493f84ea3b13214720",
+    "url": "/Texters/static/js/2.3f9b2d3a.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/Texters/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "9c769af0cee1b5612019ca36e6eeba10",
+    "url": "/Texters/index.html"
+  }
+];
