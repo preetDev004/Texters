0847ae0a325b05aa16c7baf3b7aca29e101d6061
preet patel <114741744+preetDev004@users.noreply.github.com>
2022-10-10 17:25:51 +0530

    change

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(props) {
             <footer className={`text-center text-lg-start bg-${props.mode === 'success' ? 'success' : 'light'} text-${props.mode === 'success' ? 'light' : 'muted'}`} >  
                 <div className="text-center p-4 ">
                     © 2022 Copyright :
-                    <a className="text-reset fw-bold" href="/">Texters.com</a>
+                    <a className="text-reset fw-bold" href="https://github.com/preetDev004">Goto GitHub</a>
                 </div>
             </footer>
         </div>
